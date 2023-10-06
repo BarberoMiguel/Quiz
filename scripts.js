a8804c99d2c7ba02e561905242d640d87467580c
BarberoMiguel <migomab@gmail.com>
2023-10-06 23:45:59 +0200

    página terminada y bugs arreglados

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// variables que contienen las preguntas y respuestas. Pueden tener longitud indefinida
 const quiz = [
     {"label": "What is the capital of France?",
     "answer1": "Berlin",
@@ -51,10 +52,31 @@ const quiz = [
     "answer4": "Ethiopia"}
 ];
 const answers = ["Paris","Nile","Japan","Africa","Mount Everest","Turkey","Sahara","Canberra","Iceland","South Africa"];
+
+//variables de uso interno
 var index = 0;
 var scoreArray = [];
+var score = 0;
+
+function bubbleSort(inputArr) {
+    let len = inputArr.length;
+    let swaped;
+    do {
+        swaped = false;
+        for (let i = 0; i < len-1; i++) {
+            if (inputArr[i][1] < inputArr[i + 1][1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                swaped = true;
+            }
+        }
+    } while (swaped);
+    return inputArr;
+};
 
 if (document.title == "Geography Quiz") {
+    // añadir contenido a la página
     let main = document.createElement("main");
     let section = `
         <section>
@@ -63,10 +85,13 @@ if (document.title == "Geography Quiz") {
             <a href="./Pages/quiz.html"><img src="./Assets/test-button.png" alt="button"></a>    
         </section>`;
     main.innerHTML = section;
-    document.getElementById("home").appendChild(main); 
+    document.getElementById("home").appendChild(main);
 }
+     
+
 
 if (document.title == "Geography Quiz1") {
+    //añadir contenido a la página
     let main1 = document.createElement("main");
     main1.id = "quiz";
     let section1 = `
@@ -84,25 +109,30 @@ if (document.title == "Geography Quiz1") {
                 <article id="test">
                     <h2 id="title">${quiz[index].label}</h2>
                     <article id="radio">
-                        <label for="answer1" class="answer1" id="a1">${quiz[index].answer1}</label>
-                        <input type="radio" name="answer1" id="answer1">
-                        <label for="answer2" class="answer2" id="a2">${quiz[index].answer2}</label>
-                        <input type="radio" name="answer2" id="answer2">
-                        <label for="answer3" class="answer3" id="a3">${quiz[index].answer3}</label>
-                        <input type="radio" name="answer3" id="answer3">
-                        <label for="answer4" class="answer4" id="a4">${quiz[index].answer4}</label>
-                        <input type="radio" name="answer4" id="answer4">
+                        <label for="answer1" class="answer0" id="a0">${quiz[index].answer1}</label>
+                        <input type="radio" name="answer" id="answer1">
+                        <label for="answer2" class="answer1" id="a1">${quiz[index].answer2}</label>
+                        <input type="radio" name="answer" id="answer2">
+                        <label for="answer3" class="answer2" id="a2">${quiz[index].answer3}</label>
+                        <input type="radio" name="answer" id="answer3">
+                        <label for="answer4" class="answer3" id="a3">${quiz[index].answer4}</label>
+                        <input type="radio" name="answer" id="answer4">
                     </article>
                 </article>
                 <article id="end">
-                    <h2>Submit results</h2><br><br>
+                    <h2>How did it go?</h2><br>
+                    <p>Submit results</p><br>
                     <button type="submit">Submit</button>
                 </article>
             </form>
+            <div id="cronometro">00:00:00</div>
         </section>`;
     main1.innerHTML = section1;
     document.getElementById("testPage").appendChild(main1);
 
+    // Mostrar sólo la primera pantalla
+    const crono = document.getElementById("cronometro");
+    crono.style.display = "none";
     const name = document.getElementById("name");
     name.style.display = "flex";
     const test = document.getElementById("test");
@@ -110,6 +140,25 @@ if (document.title == "Geography Quiz1") {
     const end = document.getElementById("end");
     end.style.display = "none";
 
+    //funcionamiento del cronometro
+    let tiempoInicio = 0;
+    let intervalo;
+    function actualizarCronometro() {
+        const tiempoActual = Date.now();
+        const diferencia = tiempoActual - tiempoInicio;
+      
+        const horas = Math.floor(diferencia / 3600000);
+        const minutos = Math.floor((diferencia % 3600000) / 60000);
+        const segundos = Math.floor((diferencia % 60000) / 1000);
+      
+        const tiempoFormateado = `${String(horas).padStart(2, "0")}:${String(
+          minutos
+        ).padStart(2, "0")}:${String(segundos).padStart(2, "0")}`;
+      
+        document.getElementById("cronometro").textContent = tiempoFormateado;
+      }
+
+    // Validación formulario nombre
     let button1 = document.getElementById("formName");
     button1.addEventListener("submit", function(event) {
         const user = event.target.user.value;
@@ -122,41 +171,163 @@ if (document.title == "Geography Quiz1") {
         } else {
             event.preventDefault();
             scoreArray.push(user);
+            crono.style.display = "block";
             name.style.display = "none";
             test.style.display = "flex";
+            if (!intervalo) {
+                tiempoInicio = Date.now();
+                intervalo = setInterval(actualizarCronometro, 1000);
+              }
         };
     });
 
+    
     let inputAnswers = document.querySelectorAll("#radio input");
-    let labelAnswers = document.querySelectorAll("#radio label");
-    console.log(inputAnswers);
+    let labelAnswers = document.querySelectorAll("#radio label"); 
+    let opcionUnica = true; //solo deja pulsar el botón una vez
 
+    //función cambiar color
+    function color() {
+        for (let k = 0; k < labelAnswers.length; k++) {
+            if (labelAnswers[k].textContent == answers[index]) {
+                document.getElementById(`a${k}`).classList.toggle(`answer${k}`);
+                document.getElementById(`a${k}`).classList.toggle("green");
+            } else {
+                document.getElementById(`a${k}`).classList.toggle(`answer${k}`);
+                document.getElementById(`a${k}`).classList.toggle("red");
+            }; 
+        }; 
+    };
+    
+
+    //añadir botones que checkeen los radio
+    //inhabilitar las opciones durante 2 segundos para que no se pueda cambiar
+    //cambiar los colores para mostrar la respuesta correcta    
+    for (let i = 0; i < labelAnswers.length; i++) {
+        labelAnswers[i].addEventListener("click", function(event) {
+            inputAnswers[i].checked = true;
+            if (opcionUnica) {
+                opcionUnica= false;
+                color();
+                for (let j = 0; j < inputAnswers.length; j++) {
+                    inputAnswers[j].disabled = true;
+                }
+                function after() {
+                    inputAnswers[i].checked = false;
+                    color();
+                    opcionUnica = true;
+                    for (let j = 0; j < inputAnswers.length; j++) {
+                        inputAnswers[j].disabled = false;
+                    };
+                    index += 1;
+                    if (index == 10) {
+                        //mostrar siguiente pantalla
+                        test.style.display = "none";
+                        end.style.display = "flex";
+                        //parar cronometro
+                        clearInterval(intervalo);
+                        intervalo = null;
+                    } else {
+                        document.getElementById("title").textContent = `${quiz[index].label}`;
+                        document.getElementById("a0").textContent = `${quiz[index].answer1}`;
+                        document.getElementById("a1").textContent = `${quiz[index].answer2}`;
+                        document.getElementById("a2").textContent = `${quiz[index].answer3}`;
+                        document.getElementById("a3").textContent = `${quiz[index].answer4}`;
+                    }
+                };
+                if (labelAnswers[i].textContent == answers[index]) {
+                    score += 1;
+                }
+                setTimeout(after, 2000);
+            }
+            
+        });
+    };
+    //variable de control
+    var control = false;
+    let formTest = document.getElementById("formTest");
+    formTest.addEventListener("submit", function(event) {
+        //recupero el scoreBoard
+        let variableRecuperada = localStorage.getItem("scoreBoard");
+        variableRecuperada = JSON.parse(variableRecuperada);
+
+        //preparo la variable para introducir y la introduzco.
+        scoreArray.push(score);
+        variableRecuperada.push(scoreArray);
+
+        //ordeno el scoreboard y lo vuelvo a guardar
+        bubbleSort(variableRecuperada);
+        variableRecuperada = JSON.stringify(variableRecuperada);
+        localStorage.setItem('scoreBoard', variableRecuperada);
+
+        //guardo también la puntuación del último para felicitarle
+        scoreArray = JSON.stringify(scoreArray);
+        localStorage.setItem('lastUser', scoreArray);
+    }); 
+};
+
+if (document.title == "Geography Quiz2") {
+    //recupero parámetros
     let variableRecuperada = localStorage.getItem("scoreBoard");
     variableRecuperada = JSON.parse(variableRecuperada);
-    console.log(variableRecuperada);
+    let scoreArray1 = localStorage.getItem("lastUser");
+    scoreArray1 = JSON.parse(scoreArray1);
+    let user = scoreArray1[0];
+    let score = scoreArray1[1];
 
-    document.getElementById("a1").classList.toggle("answer1");
-    document.getElementById("a1").classList.toggle("red");
+    //pinto la página
+    let main2 = document.createElement("main");
+    let section2 = `
+    <section>
+        <h2>Well done ${user}!!</h2>
+        <h2>Your score is: ${score} / 10</h2>
+        <p id="result">Perfect!!</p>
+        <img src="../Assets/10-.png" alt="image" id="imageFinal">
 
-    document.getElementById("a1").classList.toggle("answer1");
-    document.getElementById("a1").classList.toggle("red");
+        <h2>ScoreBoard</h2>
+        <table>
+            <tr>
+               <th>Name</th> 
+               <th>Score</th>
+            </tr>`;
+    for (let i = 0; i < variableRecuperada.length; i++) {
+        section2 += `
+            <tr>
+                <td>${variableRecuperada[i][0]}</td>
+                <td>${variableRecuperada[i][1]}</td>
+            </tr>`;  
+    }
+    section2 += `
+        </table>
+    </section>`;
+    main2.innerHTML = section2;
+    document.getElementById("solPage").appendChild(main2);
+    debugger
+    //cambio texto e imagen según la puntuación
+    let result = document.getElementById("result");
+    let imgUrl = document.getElementById("imageFinal");
+    switch (score) {
+        case 7:
+        case 8:
+        case 9:
+            result.textContent = "Great score!!";
+            imgUrl.src = "../Assets/7-9-.png";
+            break;
+        case 5:
+        case 6:
+            result.textContent = "Well done!! You'll do better next Time";
+            imgUrl.src = "../Assets/5-6-.png";
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            result.textContent = "Looks like you should study more";
+            imgUrl.src = "../Assets/0-4-.png";
+        default:
+            break;
+    }
+};
 
-    let bubbleSort = (inputArr) => {
-        let len = inputArr.length;
-        let swaped;
-        do {
-            swaped = false;
-            for (let i = 0; i < len; i++) {
-                if (inputArr[i] > inputArr[i + 1]) {
-                    let tmp = inputArr[i];
-                    inputArr[i] = inputArr[i + 1];
-                    inputArr[i + 1] = tmp;
-                    swaped = true;
-                }
-            }
-        } while (swaped);
-        return inputArr;
-    };
-}
 
 
